22fe41531fe53a642343def47960a5b81ed5ae33
John Aricsson Majumder <johnaricsson1@gmail.com>
2026-06-06 02:15:47 +0600

    CV download

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
 
   const handleDownloadCV = () => {
     const link = document.createElement("a");
-    link.href = "/cv.pdf";
+    link.href = "/John_Aricsson_CV.pdf";
     link.download = "John_Aricsson_CV.pdf";
     document.body.appendChild(link);
     link.click();
